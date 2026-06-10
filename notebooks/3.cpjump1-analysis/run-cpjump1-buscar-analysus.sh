ed50fba85c00197e35b9791a587179862d874b74
Erik Serrano <erikishere3@gmail.com>
2026-06-10 11:02:03 -0600

    fixed bug in shell script

--- notebooks/3.cpjump1-analysis/run-cpjump1-buscar-analysus.sh
+++ notebooks/3.cpjump1-analysis/run-cpjump1-buscar-analysus.sh
@@ -9,7 +9,7 @@ jupyter nbconvert --to script --output-dir=./nbconverted ./*.ipynb
 
 # execute all scripts
 python ./nbconverted/1.generate-on-off-signatures.py
-python ./nbconverted/2.run-compound-replicate-analysis.py
-python ./nbconverted/3.plot-replicate-consistency-analysis.py
+python ./nbconverted/2.run-compound-replicate-consistency-analysis.py
+python ./nbconverted/3.run-crispr-replicate-consistency-analysis.py
 python ./nbconverted/4.plot-replicate-consistency-analysis.py
 python ./nbconverted/5.cpjump1-titration-analysis.py
